e04bf554ec5a2996fe54e55bb0c7a4432ff061c5
rubycidev <117899216+rubycidev@users.noreply.github.com>
2024-09-10 12:39:50 +0200

    Update devise.rb add secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,5 @@
 Devise.setup do |config|
+  config.secret_key = 'd9ba5cd894719214f911c787394461caa11bb3f3b9168158114d6190887766c098699bb0ea5266ce97bdff617eb3ceb417df10541197050f15d26408864dbe47'
   config.mailer_sender = 'team@openfarm.cc'
   require 'devise/orm/mongoid'
   config.case_insensitive_keys = [ :email ]
